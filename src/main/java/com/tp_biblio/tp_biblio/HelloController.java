96302c2d8037418ac98cd5807916a492ce23aaa0
Ridel Timothe <timothe.ridel@student-cs.fr>
2024-10-14 13:55:59 +0200

    initial commit

---
+++ src/main/java/com/tp_biblio/tp_biblio/HelloController.java
@@ -0,0 +1,14 @@
+package com.tp_biblio.tp_biblio;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module com.tp_biblio.tp_biblio {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+
+    opens com.tp_biblio.tp_biblio to javafx.fxml;
+    exports com.tp_biblio.tp_biblio;
+}
