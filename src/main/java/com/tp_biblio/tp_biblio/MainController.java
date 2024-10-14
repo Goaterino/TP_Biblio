5698bed0374791f6214cf801937d9bb95f17dfee
Ridel Timothe <timothe.ridel@student-cs.fr>
2024-10-14 14:55:30 +0200

    added mysql-connector-j-8.0.33.jar and renamed things

--- src/main/java/com/tp_biblio/tp_biblio/MainController.java
+++ src/main/java/com/tp_biblio/tp_biblio/MainController.java
@@ -1,9 +1,10 @@
 package com.tp_biblio.tp_biblio;
 
 import javafx.fxml.FXML;
+import java.sql.*;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class MainController {
     @FXML
     private Label welcomeText;
 
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,7 @@ module com.tp_biblio.tp_biblio {
 
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
+    requires java.sql;
 
     opens com.tp_biblio.tp_biblio to javafx.fxml;
     exports com.tp_biblio.tp_biblio;
